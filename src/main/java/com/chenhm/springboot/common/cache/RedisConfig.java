f77d5a39eeb215fa9bf92a6dcb9e4d9842666700
chenhongmin <Hongmin.Chen1@geely.com>
2018-08-16 09:31:19 +0800

    spring boot 添加redis 支持

--- src/main/java/com/chenhm/springboot/common/cache/RedisConfig.java
+++ src/main/java/com/chenhm/springboot/common/cache/RedisConfig.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -22,6 +23,8 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    @Autowired
+    private ObjectMapper objectMapper;
     /**
      * redisTemplate 序列化使用的jdk serializer, 存储二进制字节码, 所以自定义序列化类
      *
